28066847bfff74c5a662569ffb3aac9366d4f42b
Mao Rivera Sanchez Armas <maorivera@outlook.com>
2017-01-02 15:43:03 -0600

    Preparando el sistema para Git

---
+++ src/sanfelipe/SanFelipe.java
@@ -0,0 +1,17 @@
+
+package sanfelipe;
+
+/**
+ *
+ * @author Mao
+ */
+public class SanFelipe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
